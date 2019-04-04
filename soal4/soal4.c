16ad05ac26ecb86b3b70e9edcdc8a9cdf25f73d4
Raden Bimo Rizki Prayogo <37038492+Capetian@users.noreply.github.com>
2019-04-04 16:09:05 +0700

    Update soal4.c

--- soal4/soal4.c
+++ soal4/soal4.c
@@ -9,7 +9,33 @@ int mutex = 0, step = 1;
 pthread_t tid[6];
 typedef struct s_arg{int n;} arg;
 
-void* factorial( void* args)
+void* Step( void* args);
+
+int main(int argc, char** argv) {
+
+	int i,n;
+
+    arg args[argc];
+ 
+	for ( i = 0; i < 10 ; i++){
+		pthread_create(&tid[i%2], NULL, &Step, NULL );
+		if (i%2==1) {
+		    int j;
+		    for ( j = 0; j <2; j++){
+			pthread_join (tid[j], NULL);
+		    }
+		    step++;
+		    mutex = 0;
+		}
+        
+	}
+	pthread_exit(NULL);
+	return 0;
+}
+
+
+
+void* Step( void* args)
 {
     int  n = ++mutex;
     char dir[26] = "~/Documents/FolderProses";
@@ -25,7 +51,6 @@ void* factorial( void* args)
         }
     }
     
-
     char filen[30] = "SimpanProses";
     sprintf(filen,"%s%d.txt", filen, n);
 
@@ -70,30 +95,4 @@ void* factorial( void* args)
     sprintf(step5,"%s %s/%s -d %s", step5, dir, zip, dir);
     system(step5);
 
-
-}
-
-int main(int argc, char** argv) {
-
-	int i,n;
-
-    arg args[argc];
- 
-	for ( i = 0; i < 10 ; i++){
-        pthread_create(&tid[i%2], NULL, &factorial, NULL );
-        if (i%2==1) {
-            int j;
-            for ( j = 0; j <2; j++){
-                pthread_join (tid[j], NULL);
-            }
-            step++;
-            mutex = 0;
-        }
-        
-	}
-
-
-
-	pthread_exit(NULL);
-	return 0;
 }
